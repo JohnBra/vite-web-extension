75f0458876bee4ea353983eafb5d2a695d2999f1
Jonathan Braat <jonathan.braat@live.com>
2022-05-12 22:42:36 +1200

    [FIX] popup root element id

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -5,7 +5,7 @@ import '@assets/styles/tailwind.css';
 import Popup from '@pages/popup/Popup';
 
 function init() {
-  const rootContainer = document.querySelector("#app-container");
+  const rootContainer = document.querySelector("#_root");
   if (!rootContainer) throw new Error("Can't find Popup root element");
   const root = createRoot(rootContainer);
   root.render(<Popup />);
