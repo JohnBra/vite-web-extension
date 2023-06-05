92d09d19fc2e85d03dff0b951601db9bae2686ac
Himanshu Patil <himanshupatil744@gmail.com>
2023-06-05 11:00:32 +0530

    removed sass dependency

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import './style.scss' 
+import './style.css' 
 const div = document.createElement('div');
 div.id = '__root';
 document.body.appendChild(div);
