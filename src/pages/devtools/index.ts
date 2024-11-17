ca9b350e6730145b2b64485310983088dc7ee6a9
Jonathan Braat <jonathan.braat@live.com>
2024-11-18 10:52:30 +1300

    [FIX] devtools panel

--- src/pages/devtools/index.ts
+++ src/pages/devtools/index.ts
@@ -3,5 +3,5 @@ import Browser from 'webextension-polyfill';
 Browser
   .devtools
   .panels
-  .create('Dev Tools', 'icon-32.png', 'src/pages/panel/index.html')
+  .create('Dev Tools', 'icon-32.png', 'src/pages/devtools/index.html')
   .catch(console.error);
